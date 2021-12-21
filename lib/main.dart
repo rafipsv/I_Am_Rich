ab0fa9a35d4e735500ed308c6e766b729500f503
Muhammad Fazlul Karim <rafidiya143@gmail.com>
2021-12-21 10:48:00 +0600

    I Am Rich App Completed

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Center(
+            child: Text(
+              "I Am Rich",
+            ),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/diamond.png"),
+          ),
+        ),
+      ),
+    );
+  }
+}
